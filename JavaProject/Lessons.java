822c242c0f717da456e85c8e6ed18cfb26a405f7
petruho2019 <vladgolovko20062018@mail.ru>
2024-03-11 15:17:03 +0300

    --amend test

--- JavaProject/Lessons.java
+++ JavaProject/Lessons.java
@@ -5,5 +5,6 @@ class Lessons
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello new commit");
+        System.out.println("--amend");
     }
 }
